e848d435478abb79ab63149ec51bc62bb2495737
Jhon <165972049+MINORURAKUEN@users.noreply.github.com>
2026-03-05 14:19:22 -0500

    Refactor Google Drive downloader to use fetch

--- plugins/downloader-gdrive.js
+++ plugins/downloader-gdrive.js
@@ -1,74 +1,48 @@
-import {join} from 'path';
-import {promises as fs} from 'fs';
-import {promisify} from 'util';
-import {google} from 'googleapis';
+import fetch from 'node-fetch';
+import {sizeFormatter} from 'human-readable';
 
+const formatSize = sizeFormatter({
+  std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B`});
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = join(__dirname, '..', 'token.json');
+const handler = async (m, {conn, args, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+  const tradutor = _translate.plugins.descargas_gdrive
 
-class GoogleAuth extends EventEmitter {
-  constructor() {
-    super();
+  if (!args[0]) throw `${tradutor.texto1} _${usedPrefix + command} https://drive.google.com/file/d/1dmHlx1WTbH5yZoNa_ln325q5dxLn1QHU/view_`;
+  try {
+    GDriveDl(args[0]).then(async (res) => {
+      conn.reply(m.chat, tradutor.texto2, m);
+      if (!res) throw res;
+      conn.sendFile(m.chat, res.downloadUrl, res.fileName, '', m, null, {mimetype: res.mimetype, asDocument: true});
+    });
+  } catch (e) {
+    m.reply(tradutor.texto3);
+    console.log(e);
   }
-
-  async authorize(credentials) {
-    let token;
-    const {client_secret, client_id} = credentials;
-    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, `http://localhost:${port}`);
-    try {
-      token = JSON.parse(await fs.readFile(TOKEN_PATH));
-    } catch (e) {
-      const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
-      });
-      this.emit('auth', authUrl);
-      const code = await promisify(this.once).bind(this)('token');
-      token = await oAuth2Client.getToken(code);
-      await fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-    } finally {
-      await oAuth2Client.setCredentials(token);
-    }
-  }
-
-  token(code) {
-    this.emit('token', code);
-  }
-}
-
-class GoogleDrive extends GoogleAuth {
-  constructor() {
-    super();
-    this.path = '/drive/api';
-  }
-
-  async getFolderID(path) {
-
-  }
-
-  async infoFile(path) {
-
-  }
-
-  async folderList(path) {
-
-  }
-
-  async downloadFile(path) {
-
-  }
-
-  async uploadFile(path) {
-
-  }
-}
-
-export {
-  GoogleAuth,
-  GoogleDrive,
 };
+handler.command = /^(gdrive)$/i;
+export default handler;
+async function GDriveDl(url) {
+  let id;
+  if (!(url && url.match(/drive\.google/i))) throw 'Invalid URL';
+  id = (url.match(/\/?id=(.+)/i) || url.match(/\/d\/(.*?)\//))[1];
+  if (!id) throw 'ID Not Found';
+  const res = await fetch(`https://drive.google.com/uc?id=${id}&authuser=0&export=download`, {
+    method: 'post',
+    headers: {
+      'accept-encoding': 'gzip, deflate, br',
+      'content-length': 0,
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      'origin': 'https://drive.google.com',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+      'x-client-data': 'CKG1yQEIkbbJAQiitskBCMS2yQEIqZ3KAQioo8oBGLeYygE=',
+      'x-drive-first-party': 'DriveWebUi',
+      'x-json-requested': 'true'}});
+  const {fileName, sizeBytes, downloadUrl} = JSON.parse((await res.text()).slice(4));
+  if (!downloadUrl) throw 'Link Download Limit!';
+  const data = await fetch(downloadUrl);
+  if (data.status !== 200) throw data.statusText;
+  return {downloadUrl, fileName, fileSize: formatSize(sizeBytes), mimetype: data.headers.get('content-type')};
+}
